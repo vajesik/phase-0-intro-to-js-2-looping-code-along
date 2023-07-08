639c327ed2ecb5d7106db7b0009f053f97439995
Vanessa Jesik <vajesik@gmail.com>
2023-07-07 22:25:45 -0600

    learning to loop

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+const guests = ["Charlie", "Samip", "Ali"]
+const birthdayThankYouCards = []
+
+function writeCards(guests, event) {
+    for (let x = 0; x < guests.length; x++) {
+        birthdayThankYouCards[x] = `Thank you, ${guests[x]}, for the wonderful ${event} gift!`;
+    }
+    return birthdayThankYouCards
+}
+
+writeCards(guests, "surprise")
+
+
+function countDown(y) {while (y>=0) {console.log(y--)}}
